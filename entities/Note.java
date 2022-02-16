6da67dec345975e13dd42086ff3c4cd4424768be
João Pedro Mesquita <jp.devdroid@gmail.com>
2022-02-16 14:32:36 -0300

    Class Note constructor, getters and setters

--- entities/Note.java
+++ entities/Note.java
@@ -1,9 +1,51 @@
 package entities;
 
+import java.util.Date;
+
 public class Note {
 
-	public Note() {
-		
+	private String status;
+	private String body;
+	private String observation;
+	private Date hour;
+	
+	public Note(String status, String body, String observation) {
+		this.status = status;
+		this.body = body;
+		this.observation = observation;
 	}
 
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getObservation() {
+		return observation;
+	}
+
+	public void setObservation(String observation) {
+		this.observation = observation;
+	}
+
+	public Date getHour() {
+		return hour;
+	}
+
+	public void setHour(Date hour) {
+		this.hour = hour;
+	}
+	
+	
 }
